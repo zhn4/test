a33881a3b0378d4ea88e33c874e25392d8019155
zhou <pleomax4@126.com>
2019-10-21 18:24:50 +0800

    new file

---
+++ rebuild.js
@@ -0,0 +1,114 @@
+const crypto = require("crypto")
+const axios = require("axios")
+
+const url = 'https://dm.aliyuncs.com/'
+
+module.exports = function (config, cb) {
+  config = config || {}
+
+  const nonce = Date.now(),
+        date = new Date(),
+        errorMsg = []
+
+  if (!config.accessKeyID) {
+    errorMsg.push('accessKeyID required')
+  }
+  if (!config.accessKeySecret) {
+    errorMsg.push('accessKeySecret required')
+  }
+  if (!config.accountName) {
+    errorMsg.push('accountName required')
+  }
+
+  let param = {
+    AccessKeyId: config.accessKeyID,
+    Format: 'JSON',
+    AccountName: config.accountName,
+    AddressType: typeof config.addressType == 'undefined' ? 0 : config.addressType,
+    SignatureMethod: 'HMAC-SHA1',
+    SignatureNonce: nonce,
+    SignatureVersion: '1.0',
+    TemplateCode: config.templateCode,
+    Timestamp: date.toISOString(),
+    Version: '2015-11-23'
+  }
+  switch(config.action) {
+    case 'single': {
+      if (!config.toAddress) {
+        errorMsg.push('toAddress required')
+      }
+
+      param.Action = 'single'
+      param.ReplyToAddress = !!config.replyToAddress
+      param.ToAddress = config.toAddress
+
+      if (config.fromAlias) {
+        param.FromAlias = config.fromAlias
+      }
+      if (config.subject) {
+        param.Subject = config.subject
+      }
+      if (config.htmlBody) {
+        param.HtmlBody = config.htmlBody
+      }
+      if (config.textBody) {
+        param.TextBody = config.textBody
+      }
+      break
+    }
+    case 'batch': {
+      if (!config.templateName) {
+        errorMsg.push('templateName required')
+      }
+      if (!config.receiversName) {
+        errorMsg.push('receiversName required')
+      }
+
+      param.Action = 'batch'
+      param.TemplateName = config.templateName
+      param.ReceiversName = config.receiversName
+      
+      if (config.tagName) {
+        param.TagName = config.tagName
+      }
+      break
+    }
+    default: {
+      cb('error action', null)
+      break
+    }
+  }
+
+  if (errorMsg.length) {
+    return cb(errorMsg.join(','))
+  }
+  
+  var signStr = []
+  for (var i in param) {
+    signStr.push(encodeURIComponent(i) + '=' + encodeURIComponent(param[i]))
+  }
+  signStr.sort()
+  signStr = signStr.join('&')
+  signStr = 'POST&%2F&' + encodeURIComponent(signStr)
+  const sign = crypto.createHmac("sha1", config.accessKeySecret + '&')
+    .update(signStr)
+    .digest('base64')
+  const signature = encodeURIComponent(sign)
+  var reqBody = ['Signature=' + signature]
+  for (var i in param) {
+    reqBody.push(i + '=' + param[i])
+  }
+  reqBody = reqBody.join('&')
+
+  axios({
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    uri: url,
+    body: reqBody,
+    method: 'POST'
+  }, function (err, res, body) {
+    cb(err, body)
+  })
+
+}
