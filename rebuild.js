f2a24cbd99d980ec5c619d994fdaa0bc1891c88f
zhou <pleomax4@126.com>
2019-10-22 18:23:56 +0800

    去掉所有分号

--- rebuild.js
+++ rebuild.js
@@ -1,7 +1,7 @@
-const crypto = require("crypto");
-const axios = require("axios");
+const crypto = require("crypto")
+const axios = require("axios")
 
-const url = "https://dm.aliyuncs.com/";
+const url = "https://dm.aliyuncs.com/"
 
 function request(url, reqBody) {
   return new Promise((resolve, reject) => {
@@ -15,32 +15,32 @@ function request(url, reqBody) {
     })
     .then((res) => {
       if(res.data.success) {
-        resolve(res);
+        resolve(res)
       }else {
-        reject(res);
+        reject(res)
       }
     })
     .catch((err) => {
-      reject(err);
-    });
-  });
+      reject(err)
+    })
+  })
 }
 
 module.exports.default = function(config, cb) {
-  config = config || {};
+  config = config || {}
 
   const nonce = Date.now(),
         date = new Date(),
-        errorMsg = [];
+        errorMsg = []
 
   if (!config.accessKeyID) {
-    errorMsg.push("accessKeyID required");
+    errorMsg.push("accessKeyID required")
   }
   if (!config.accessKeySecret) {
-    errorMsg.push("accessKeySecret required");
+    errorMsg.push("accessKeySecret required")
   }
   if (!config.accountName) {
-    errorMsg.push("accountName required");
+    errorMsg.push("accountName required")
   }
 
   let param = {
@@ -54,72 +54,72 @@ module.exports.default = function(config, cb) {
     TemplateCode: config.templateCode,
     Timestamp: date.toISOString(),
     Version: "2015-11-23"
-  };
+  }
   switch(config.action) {
     case "single":
       if (!config.toAddress) {
-        errorMsg.push("toAddress required");
+        errorMsg.push("toAddress required")
       }
 
-      param.Action = "single";
-      param.ReplyToAddress = !!config.replyToAddress;
-      param.ToAddress = config.toAddress;
+      param.Action = "single"
+      param.ReplyToAddress = !!config.replyToAddress
+      param.ToAddress = config.toAddress
 
       if (config.fromAlias) {
-        param.FromAlias = config.fromAlias;
+        param.FromAlias = config.fromAlias
       }
       if (config.subject) {
-        param.Subject = config.subject;
+        param.Subject = config.subject
       }
       if (config.htmlBody) {
-        param.HtmlBody = config.htmlBody;
+        param.HtmlBody = config.htmlBody
       }
       if (config.textBody) {
-        param.TextBody = config.textBody;
+        param.TextBody = config.textBody
       }
-      break;
+      break
     case "batch":
       if (!config.templateName) {
-        errorMsg.push("templateName required");
+        errorMsg.push("templateName required")
       }
       if (!config.receiversName) {
-        errorMsg.push("receiversName required");
+        errorMsg.push("receiversName required")
       }
 
-      param.Action = "batch";
-      param.TemplateName = config.templateName;
-      param.ReceiversName = config.receiversName;
+      param.Action = "batch"
+      param.TemplateName = config.templateName
+      param.ReceiversName = config.receiversName
       
       if (config.tagName) {
-        param.TagName = config.tagName;
+        param.TagName = config.tagName
       }
-      break;
+      break
     default:
-      cb("error action", null);
-      break;
+      cb("error action", null)
+      break
   }
 
   if (errorMsg.length) {
-    return cb(errorMsg.join(","));
+    return cb(errorMsg.join(","))
   }
   
-  let signStr = [];
+  let signStr = []
   for (let i in param) {
-    signStr.push("".concat(encodeURIComponent(i), "=", encodeURIComponent(param[i])));
+    signStr.push("".concat(encodeURIComponent(i), "=", encodeURIComponent(param[i])))
   }
-  signStr.sort();
-  signStr = signStr.join("&");
-  signStr = "POST&%2F&" + encodeURIComponent(signStr);
+  signStr.sort()
+  signStr = signStr.join("&")
+  signStr = "POST&%2F&" + encodeURIComponent(signStr)
   const sign = crypto.createHmac("sha1", config.accessKeySecret + "&")
     .update(signStr)
-    .digest("base64");
-  const signature = encodeURIComponent(sign);
-  let reqBody = ["Signature=" + signature];
+    .digest("base64")
+  const signature = encodeURIComponent(sign)
+  let reqBody = ["Signature=" + signature]
   for (let i in param) {
-    reqBody.push("".concat(i, "=", param[i]));
+    reqBody.push("".concat(i, "=", param[i]))
   }
-  reqBody = reqBody.join("&");
+  reqBody = reqBody.join("&")
 
-  request(url, reqBody);
+  request(url, reqBody)
 
-};
+}
